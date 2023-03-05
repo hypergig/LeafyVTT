f01afde557eb004540ce5661958e98b4eab75914
Jordan Cohen <hypergig@gmail.com>
2023-03-04 21:51:16 -0500

    map scaling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Map from './Map'
 
 function App() {
     return (
-        <Map/>
+        <Map imgUrl={"https://i.pinimg.com/originals/a2/04/d4/a204d4a2faceb7f4ae93e8bd9d146469.jpg"}
+             gridPx={72} offsetHeightPx={30} offsetWidthPx={30}/>
     );
 }
 
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,11 +1,55 @@
+import React from "react";
+import {CRS, LatLngBounds} from "leaflet";
 import "leaflet/dist/leaflet.css";
-import {MapContainer} from "react-leaflet";
+import {ImageOverlay, MapContainer} from "react-leaflet";
 import '@geoman-io/leaflet-geoman-free';
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
 
-function Map() {
+interface Props {
+    children?: React.ReactNode;
+    /** background base image of the map */
+    imgUrl: string;
+    /** grid size in pixels */
+    gridPx: number
+    /** grid height offset in pixels */
+    offsetHeightPx: number
+    /** grid width offset in pixels */
+    offsetWidthPx: number
+}
+
+interface Boundaries {
+    map: LatLngBounds
+    img: LatLngBounds
+}
+
+function Map(props: Props) {
+    const [boundaries, setboundaries] = React.useState<undefined | Boundaries>()
+
+    React.useEffect(() => {
+        const img = new Image()
+        img.onload = () => {
+            const offsetHeight = (props.offsetHeightPx / props.gridPx) % 1
+            const offsetWidth = (props.offsetWidthPx / props.gridPx) % 1
+            setboundaries({
+                map: new LatLngBounds([[0, 0],
+                    [Math.ceil(img.height / props.gridPx), Math.ceil(img.width / props.gridPx)]]),
+                img: new LatLngBounds([[offsetHeight, offsetWidth],
+                    [offsetHeight + (img.height / props.gridPx), offsetWidth + (img.width / props.gridPx)]])
+            })
+        }
+        img.src = props.imgUrl
+    }, [props])
+
+    if (!boundaries) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+
     return (
-        <MapContainer id={"map"}>
+        <MapContainer id={"map"} crs={CRS.Simple} bounds={boundaries.map}>
+            <ImageOverlay url={props.imgUrl} bounds={boundaries.img}/>
+            {props.children}
         </MapContainer>
     )
 }
