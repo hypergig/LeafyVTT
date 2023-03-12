84250f9bc78570c203931c2836b26885dd0358f4
Jordan Cohen <hypergig@gmail.com>
2023-03-12 15:56:55 -0400

    more tokens to play with

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import Token from "./Token";
 import {LatLng} from "leaflet";
 
 function App() {
+    const img = "https://i.redd.it/ew8lwfx2o1361.png"
     return (
         <Map imgUrl={"https://i.pinimg.com/originals/a2/04/d4/a204d4a2faceb7f4ae93e8bd9d146469.jpg"}
              gridPx={72} offsetHeightPx={42} offsetWidthPx={38}>
-            <Token imgUrl={'https://i.redd.it/ew8lwfx2o1361.png'} latLng={new LatLng(10, 12)}
-                   size={1}/>
+            <Token key={"1"} imgUrl={img} latLng={new LatLng(10, 12)} size={1}/>
+            <Token key={"2"} imgUrl={img} latLng={new LatLng(10, 14)} size={1}/>
+            <Token key={"3"} imgUrl={img} latLng={new LatLng(12, 12)} size={1}/>
+            <Token key={"4"} imgUrl={img} latLng={new LatLng(12, 14)} size={1}/>
         </Map>
     );
 }
